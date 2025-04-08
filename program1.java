165f47ea50c3ec46624a4bec5419fcfee057156b
Nikitasingh25 <bpsnps25@gmail.com>
2025-04-08 10:57:19 +0530

    js

---
+++ program1.java
@@ -0,0 +1,20 @@
+public class program1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hi I am Nikita Singh!! ");
+        
+    }
+};
+
+
+git push --set-upstream origin main
+
+
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Nikitasingh25/Java.git
+git push -u origin main
