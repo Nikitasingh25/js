e239db597fca1c0ebea45601cf80e290735d3069
Nikitasingh25 <bpsnps25@gmail.com>
2025-04-09 16:00:26 +0530

    java

---
+++ CompareTwoNo.java
@@ -0,0 +1,15 @@
+public class CompareTwoNo {
+    public static void main(String[] args) {
+    System.out.println("enter a no.!:");
+        int a=5;
+        System.out.println("enter th other no.!:");
+        int b=6;
+        if(a<b)
+        {
+            System.out.println("b is greater.");
+        }
+        else
+           System.out.println("a is greater.");
+    }
+    
+}
