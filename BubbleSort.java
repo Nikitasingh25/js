744ee8252d38dad3a6dbfb4ea7f463d65b67d246
Nikitasingh25 <bpsnps25@gmail.com>
2025-04-28 12:27:42 +0530

    ARRAY

---
+++ BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public static void bubble(int[] arr)
+    {
+        for(int i=0;i<arr.length-1;i++)
+         {
+            for(int j=0;j<arr.length-i-1;j++)
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+         }
+         for(int i :arr)
+         {
+            System.out.println(i);
+         }
+
+    }public static void main(String[] )
+}
