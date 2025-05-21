a4755e74f95ea76923d48eb84b3a01a19bacce0d
Nikita Singh <bpsnps25@gmail.com>
2025-05-21 14:28:54 +0530

    java

--- Interface.java
+++ Interface.java
@@ -1,5 +1,6 @@
 // To use methods of interface through implements keyword with the child class 
-//Multiple inheritance is achieved through 
+//Multiple inheritance is achieved through interface concept
+//Abstract classes contain  both normal as we as abstract methods whereas interface contains only abstract methods .
 
 interface InnerInterface {
     abstract void print();
