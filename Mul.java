46c2bcfccd06e8ae969d745edacda226e27088f6
Nikita Singh <bpsnps25@gmail.com>
2025-06-12 16:12:04 +0530

    FlatMap

---
+++ Mul.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Mul {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2, 21, 40, 49, 50);
+        Integer mul = list.stream().reduce(1, (a, b) -> a * b);
+        System.out.println(mul);
+    }
+}
