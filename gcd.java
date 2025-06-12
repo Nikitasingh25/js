36062534037c01dda5d06d5fb12d09fc0056e041
Nikita Singh <bpsnps25@gmail.com>
2025-06-12 15:43:17 +0530

    method

---
+++ gcd.java
@@ -0,0 +1,16 @@
+public class gcd {
+
+    static int gcdCalcu(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a = 12, b = 18;
+        System.out.println("GCD of " + a + " and " + b + " is: " + gcdCalcu(a, b));
+    }
+}
