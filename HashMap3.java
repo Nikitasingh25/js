abcb48ba23e03ab5c6935b4bc64255f8ced00223
Nikita Singh <bpsnps25@gmail.com>
2025-06-12 15:52:09 +0530

    word frequency

---
+++ HashMap3.java
@@ -0,0 +1,16 @@
+
+import java.util.HashMap;
+
+public class HashMap3 {
+
+    public static void main(String[] args) {
+        String s = "programming";
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] a = s.toCharArray();
+
+        for (char c : a) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        System.out.println(map);
+    }
+}
