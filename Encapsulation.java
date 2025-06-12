5aa86aeed2a53113c04ae6b631794b4d019e143b
Nikita Singh <bpsnps25@gmail.com>
2025-06-12 15:37:49 +0530

    Atm

---
+++ Encapsulation.java
@@ -0,0 +1,20 @@
+
+class ATM {
+    private int a;
+
+    public void setMoney(int a) {
+        this.a = a;
+    }
+
+    public int getMoney() {
+        return a;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        ATM a = new ATM();
+        a.setMoney(2000);
+        System.out.println(a.getMoney());
+    }
+}
