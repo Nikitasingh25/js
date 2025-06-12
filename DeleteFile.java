ac7d235dd99f2965ae4838a7f18222399d06a6f5
Nikita Singh <bpsnps25@gmail.com>
2025-06-12 16:35:49 +0530

    delete the file

---
+++ DeleteFile.java
@@ -0,0 +1,13 @@
+
+import java.io.File;
+
+public class DeleteFile {
+    public static void main(String[] args) {
+        File file = new File("create.java");
+        if (file.delete()) {
+            System.out.println("deleted succesfully");
+        } else {
+            System.out.println("failed to delete");
+        }
+    }
+}
